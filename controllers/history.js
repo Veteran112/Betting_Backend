c12a233ce670341ff4335d294d4aa8370949e32c
wileyyong <wiley.yong@gmail.com>
2023-01-18 14:28:13 -0600

    fixed hisotry api

--- controllers/history.js
+++ controllers/history.js
@@ -1,9 +1,9 @@
-const { History } = require("../models/sequelizer");
+const { Histories } = require("../models/sequelizer");
 
 const Controller = {
   async getList(req, res) {
     try {
-      var data = await History.findAll();
+      var data = await Histories.findAll();
       res.send(data);
     } catch (error) {
       res.status(500).send({ error: "Server error" });
--- routes/history.js
+++ routes/history.js
@@ -4,6 +4,6 @@ const history = require("../controllers/history");
 const router = express.Router();
 const tokenizer = require("../middlewares/tokenizer");
 
-router.get("/", history.getList);
+router.get("/", tokenizer.tokenAccess, history.getList);
 
 module.exports = router;
